9f32eefb9ce0804b742b2c71a9cf32a9672043da
BreckEmert <breck.emert@ku.edu>
2024-07-04 11:04:46 -0500

    Implemented strong replay buffer

--- Splendor/RL/random_model.py
+++ Splendor/RL/random_model.py
@@ -9,23 +9,19 @@ class RandomAgent:
         self.state_size = 241 # Size of state vector
         self.action_size = 61 # Maximum number of actions 
 
-        self.memory = self.load_memories(memories_path)
+        self.memory = self.load_memories(memories_path) if memories_path else deque(maxlen=50_000)
     
     def load_memories(self, memories_path):
-        if memories_path:
-            import pickle
-            with open(memories_path, 'rb') as f:
-                flattened_memories = pickle.load(f)
-            loaded_memories = [mem for mem in flattened_memories]
-            print(f"Loading {len(loaded_memories)} memories")
-        else:
-            loaded_memories = [[0, 0, 0, 0, 0]]
-        return deque(loaded_memories, maxlen=50_000)
+        print("Loading existing memories")
+        import pickle
+        with open(memories_path, 'rb') as f:
+            flattened_memories = pickle.load(f)
+        loaded_memories = [mem for mem in flattened_memories]
+        return deque(loaded_memories, maxlen=10_000)
 
     def get_predictions(self, state, legal_mask):
         return np.where(legal_mask, np.random.rand(self.action_size), -np.inf)
 
     def remember(self, memory, legal_mask):
-        assert memory[2] is not None
         self.memory.append(memory)
         self.memory[-2].append(legal_mask)
