0127f6b24119552b91aeb0a7cca4282e2094c9e9
BreckEmert <breck_emert@yahoo.com>
2025-07-28 00:31:02 -0500

    Added selection highlights, large refactor
    Added more classes to board_geometry.py to cover commonly used calculations and positions in the code.

--- Splendor/Play/render/__init__.py
+++ Splendor/Play/render/__init__.py
@@ -1,6 +1,10 @@
 # Splendor/Play/render/__init__.py
 
-from .board_geometry import BoardGeometry, Coord
+from .board_geometry import BoardGeometry, Rect, Coord
 from .board_renderer import BoardRenderer
 from .overlay_renderer import OverlayRenderer
-from .static_renderer import draw_game_state, take_3_indices, take_2_diff_indices
+from .static_renderer import (
+    draw_game_state, 
+    take_3_indices, 
+    take_2_diff_indices
+)
